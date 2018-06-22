c6e68f2648c47deb6ffcddaca9e6de6586d7c3ad
Tina <tparkmedia@gmail.com>
2018-06-22 16:29:56 -0700

    add search in url function

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const
   axios = require('axios'),
   PORT = 3000
 
-app.listen(PORT (err) => {
+const apiClient = axios.create()
+
+app.use(express.static('public'))
+
+app.get('/search/:term', (req, res) => {
+  const apiUrl= `http://www.omdbapi.com/?s=${req.params.term}&apikey=${process.env.API_KEY}`
+  apiClient({ method: "get", url: apiUrl}).then((apiResponse) => {
+    let results = ''
+    apiResponse.data.Search.forEach((r) => {
+      const posterImg = r.Poster
+      results += `<img src="${posterImg}">`
+    })
+    res.send(results)
+    // console.log(apiResponse.search[0].poster)
+  })
+})
+
+app.listen(PORT, (err) => {
   console.log(err || `Server running on ${PORT}`)
 })
